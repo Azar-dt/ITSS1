a0a642c586799f14b6e9a0b5c99a9980b1673047
Azar-dt <ducthang9122001@gmail.com>
2023-06-09 17:16:33 +0700

    test prisma deploy

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,20 +2,19 @@
 
 import { useEffect, useState } from "react";
 
+type Post = {
+  id: number;
+  title: string;
+};
+
 const About = () => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [_posts, setPosts] = useState<any>();
+  const [posts, setPosts] = useState<Post[]>();
   useEffect(() => {
     const fetchTest = async () => {
-      // const res = await fetch("http://localhost:3000/api/post");
-      // const data = await res.json();
-      const data = [
-        {
-          id: "1",
-          name: "Store name 1",
-        },
-      ];
-      setPosts(data);
+      const res = await fetch("http://localhost:3000/api/posts");
+      const data = await res.json();
+
+      setPosts(data.posts);
     };
     fetchTest();
   }, []);
@@ -23,6 +22,14 @@ const About = () => {
     <div>
       <h1>About</h1>
       <p>This is the about page</p>
+      {posts &&
+        posts.map((post) => {
+          return (
+            <div key={post.id}>
+              <h2>{post.title}</h2>
+            </div>
+          );
+        })}
     </div>
   );
 };
