f711a9674f5546be63528b87c92e87f6227c01d6
Redgold <72465223+tmtumk@users.noreply.github.com>
2023-06-17 00:07:15 +0700

    Update UI

---
+++ app/bike/Rent/page.tsx
@@ -0,0 +1,152 @@
+"use client";
+import Header from "@/components/Header";
+import React from 'react'
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import InputLabel from '@mui/material/InputLabel';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import { useEffect, useState } from "react";
+import Grid from '@mui/material/Grid';
+import ButtonBase from '@mui/material/ButtonBase';
+import { styled } from '@mui/material/styles';
+import {
+    Button,
+    FormControl,
+    TextField,
+    Typography,
+} from "@mui/material";
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+const Img = styled('img')({
+    // margin: 'auto',
+    // display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+});
+const page = () => {
+    const [text, setText] = useState('');
+    const [age, setAge] = React.useState('');
+    const handleChange = (event: SelectChangeEvent) => {
+        setAge(event.target.value as string);
+    };
+
+    return (
+        <>
+            <Header />
+            <Container
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    padding: "100px 100px",
+                    border: "1px solid #ccc",
+                    backgroundColor: "#fff",
+                    gridGap: "30px 60px",
+                    maxWidth: "100%",
+                    maxHeight: "100%",
+                    borderRadius: 0,
+                }}
+            >
+
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}>
+                    <Typography variant="h4" sx={{ paddingBottom: "16px" }}>
+                        バイク予約
+                    </Typography>
+                    <Grid
+                        sx={{
+                            display: "grid",
+                            gridTemplateColumns: "auto",
+                            gridGap: "15px",
+                        }}>
+                        <FormControl fullWidth>
+                            <TextField
+                                name="name"
+                                // value={loginForm.name}
+                                type="text"
+                                label="ユーザー名前"
+                                variant="outlined"
+                                fullWidth
+                            />
+                        </FormControl>
+
+                        <FormControl fullWidth>
+                            <TextField
+                                name="email"
+                                // value={loginForm.email}
+                                type="text"
+                                label="メール"
+                                variant="outlined"
+                                fullWidth
+                            />
+                        </FormControl>
+
+                        <FormControl fullWidth>
+                            <TextField
+                                name="phone number"
+                                type="number"
+                                label="電話番号"
+                                variant="outlined"
+                            />
+                        </FormControl>
+
+                        <FormControl fullWidth>
+                            <TextField
+                                name="password"
+                                type="password"
+                                label="パスワード"
+                                variant="outlined"
+                            />
+                        </FormControl>
+
+                        <FormControl fullWidth>
+                            <InputLabel id="time-start">予約開始時間</InputLabel>
+                            <Select
+                                labelId="time-start"
+                                id="time-start-select"
+                                value={age}
+                                label="time"
+                                onChange={handleChange}
+                            >
+                                <MenuItem value={10}>10:00</MenuItem>
+                                <MenuItem value={11}>11:00</MenuItem>
+                                <MenuItem value={12}>12:00</MenuItem>
+                            </Select>
+                        </FormControl>
+
+                        <FormControl fullWidth>
+                            <InputLabel id="time-start">予約終了時間</InputLabel>
+                            <Select
+                                labelId="time-start"
+                                id="time-start-select"
+                                value={age}
+                                label="time"
+                                onChange={handleChange}
+                            >
+                                <MenuItem value={10}>10:00</MenuItem>
+                                <MenuItem value={11}>11:00</MenuItem>
+                                <MenuItem value={12}>12:00</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <Button variant="contained" type="submit" fullWidth>
+                            予約
+                        </Button>
+                    </Grid>
+                </Box>
+                <div>
+                    <ButtonBase sx={{ width: "400px" }}>
+                        <Img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" />
+                    </ButtonBase>
+                </div>
+            </Container >
+
+        </>
+    )
+}
+
+
+export default page
--- app/bike/page.tsx
+++ app/bike/page.tsx
@@ -1,152 +1,233 @@
 "use client";
+import * as React from 'react';
 import Header from "@/components/Header";
-import React from 'react'
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-import InputLabel from '@mui/material/InputLabel';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import { useEffect, useState } from "react";
-import Grid from '@mui/material/Grid';
+import Tabs from '@mui/base/Tabs';
+import TabsList from '@mui/base/TabsList';
+import TabPanel from '@mui/base/TabPanel';
+import { buttonClasses } from '@mui/base/Button';
+import Tab, { tabClasses } from '@mui/base/Tab';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import TtyIcon from '@mui/icons-material/Tty';
+import Typography from '@mui/material/Typography';
 import ButtonBase from '@mui/material/ButtonBase';
-import { styled } from '@mui/material/styles';
-import {
-    Button,
-    FormControl,
-    TextField,
-    Typography,
-} from "@mui/material";
-const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+import { Box, Grid } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import styled1 from "styled-components";
+import { styled } from '@mui/system';
 const Img = styled('img')({
-    // margin: 'auto',
-    // display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
+  // margin: 'auto',
+  // display: 'block',
+  maxWidth: '100%',
+  maxHeight: '100%',
 });
-const page = () => {
-    const [text, setText] = useState('');
-    const [age, setAge] = React.useState('');
-    const handleChange = (event: SelectChangeEvent) => {
-        setAge(event.target.value as string);
-    };
+const blue = {
+  50: '#F0F7FF',
+  100: '#C2E0FF',
+  200: '#80BFFF',
+  300: '#66B2FF',
+  400: '#3399FF',
+  500: '#007FFF',
+  600: '#0072E5',
+  700: '#0059B2',
+  800: '#004C99',
+  900: '#003A75',
+};
 
-    return (
-        <>
-            <Header />
-            <Container
-                sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    padding: "100px 100px",
-                    border: "1px solid #ccc",
-                    backgroundColor: "#fff",
-                    gridGap: "30px 60px",
-                    maxWidth: "100%",
-                    maxHeight: "100%",
-                    borderRadius: 0,
-                }}
+const grey = {
+  50: '#f6f8fa',
+  100: '#eaeef2',
+  200: '#d0d7de',
+  300: '#afb8c1',
+  400: '#8c959f',
+  500: '#6e7781',
+  600: '#57606a',
+  700: '#424a53',
+  800: '#32383f',
+  900: '#24292f',
+};
+
+const StyledTab = styled(Tab)`
+  font-family: IBM Plex Sans, sans-serif;
+  color: white;
+  cursor: pointer;
+  font-size: 0.875rem;
+  font-weight: bold;
+  background-color: transparent;
+  width: 100%;
+  padding: 12px;
+  margin: 6px 6px;
+  border: none;
+  border-radius: 7px;
+  display: flex;
+  justify-content: center;
+
+  &:hover {
+    background-color: ${blue[400]};
+  }
+
+  &:focus {
+    color: #fff;
+    outline: 3px solid ${blue[200]};
+  }
+
+  &.${tabClasses.selected} {
+    background-color: #fff;
+    color: ${blue[600]};
+  }
+
+  &.${buttonClasses.disabled} {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
+
+const StyledTabPanel = styled(TabPanel)`
+  width: 100%;
+  font-family: IBM Plex Sans, sans-serif;
+  font-size: 0.875rem;
+`;
+
+const StyledTabsList = styled(TabsList)(
+  ({ theme }) => `
+  min-width: 400px;
+  background-color: ${blue[500]};
+  margin-bottom: 16px;
+  border-radius: 0px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-content: space-between;
+  box-shadow: 0px 4px 8px ${theme.palette.mode === 'dark' ? grey[900] : grey[200]};
+  `,
+);
+
+const theme = createTheme({
+  typography: {
+    // Tell MUI what the font-size on the html element is.
+    htmlFontSize: 10,
+  },
+});
+
+const theme_tiltle = createTheme({
+  typography: {
+    // Tell MUI what the font-size on the html element is.
+    htmlFontSize: 4,
+  },
+});
+const Container = styled1.div`
+  position: fixed;
+  padding: 0 20px;
+  display: flex;
+  justifyContent: center;
+  alignItems: center;
+  padding: 100px 100px;
+  backgroundColor: #fff;
+  gridGap: 30px 60px;
+`;
+export default function UnstyledTabsCustomized() {
+  return (
+    <>
+      <Header />
+      <Tabs defaultValue={1}>
+        <StyledTabsList>
+          <StyledTab value={1}>ストア管理</StyledTab>
+          <StyledTab value={2}>ストア情報</StyledTab>
+        </StyledTabsList>
+        <StyledTabPanel value={1}>
+          <Container>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                wordWrap: "break-word",
+              }}
             >
+              <ThemeProvider theme={theme_tiltle}>
+                <Typography>
+                  Happy Happy Happy Motobike Rental
+                </Typography>
+              </ThemeProvider>
 
-                <Box
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}>
-                    <Typography variant="h4" sx={{ paddingBottom: "16px" }}>
-                        バイク予約
-                    </Typography>
-                    <Grid
-                        sx={{
-                            display: "grid",
-                            gridTemplateColumns: "auto",
-                            gridGap: "15px",
-                        }}>
-                        <FormControl fullWidth>
-                            <TextField
-                                name="name"
-                                // value={loginForm.name}
-                                type="text"
-                                label="ユーザー名前"
-                                variant="outlined"
-                                fullWidth
-                            />
-                        </FormControl>
+              <Box sx={{ lineHeight: 3 }}>
+                <ThemeProvider theme={theme}>
+                  <Typography>
+                    <LocationOnIcon />アドレス: Dai Co Viet, Hai Ba Trung, Ha Noi
+                  </Typography>
+                </ThemeProvider>
+              </Box>
 
-                        <FormControl fullWidth>
-                            <TextField
-                                name="email"
-                                // value={loginForm.email}
-                                type="text"
-                                label="メール"
-                                variant="outlined"
-                                fullWidth
-                            />
-                        </FormControl>
+              <Box sx={{ lineHeight: 6 }}>
+                <ThemeProvider theme={theme}>
+                  <Typography>
+                    <TtyIcon />電話番号: 0123456789
+                  </Typography>
+                </ThemeProvider>
+              </Box>
 
-                        <FormControl fullWidth>
-                            <TextField
-                                name="phone number"
-                                type="number"
-                                label="電話番号"
-                                variant="outlined"
-                            />
-                        </FormControl>
+              <Box sx={{ lineHeight: 6 }}>
+                <Grid container spacing={0.05}>
+                  <Grid item xs={2}>
+                    <Button variant="contained" color="warning" size="large">
+                      編集
+                    </Button>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Button variant="contained" color="secondary" size="large">
+                      ストア削除
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+            <div>
+              <ButtonBase sx={{ width: "600px" }}>
+                <Img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" />
+              </ButtonBase>
+            </div>
+          </Container>
+        </StyledTabPanel>
+        <StyledTabPanel value={2}>
+          <Container>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                wordWrap: "break-word",
+              }}
+            >
+              <ThemeProvider theme={theme_tiltle}>
+                <Typography>
+                  Happy Happy Happy Motobike Rental
+                </Typography>
+              </ThemeProvider>
 
-                        <FormControl fullWidth>
-                            <TextField
-                                name="password"
-                                type="password"
-                                label="パスワード"
-                                variant="outlined"
-                            />
-                        </FormControl>
+              <Box sx={{ lineHeight: 3 }}>
+                <ThemeProvider theme={theme}>
+                  <Typography>
+                    <LocationOnIcon />アドレス: Dai Co Viet, Hai Ba Trung, Ha Noi
+                  </Typography>
+                </ThemeProvider>
+              </Box>
 
-                        <FormControl fullWidth>
-                            <InputLabel id="time-start">予約開始時間</InputLabel>
-                            <Select
-                                labelId="time-start"
-                                id="time-start-select"
-                                value={age}
-                                label="time"
-                                onChange={handleChange}
-                            >
-                                <MenuItem value={10}>10:00</MenuItem>
-                                <MenuItem value={11}>11:00</MenuItem>
-                                <MenuItem value={12}>12:00</MenuItem>
-                            </Select>
-                        </FormControl>
+              <Box sx={{ lineHeight: 6 }}>
+                <ThemeProvider theme={theme}>
+                  <Typography>
+                    <TtyIcon />電話番号: 0123456789
+                  </Typography>
+                </ThemeProvider>
+              </Box>
 
-                        <FormControl fullWidth>
-                            <InputLabel id="time-start">予約終了時間</InputLabel>
-                            <Select
-                                labelId="time-start"
-                                id="time-start-select"
-                                value={age}
-                                label="time"
-                                onChange={handleChange}
-                            >
-                                <MenuItem value={10}>10:00</MenuItem>
-                                <MenuItem value={11}>11:00</MenuItem>
-                                <MenuItem value={12}>12:00</MenuItem>
-                            </Select>
-                        </FormControl>
-                        <Button variant="contained" type="submit" fullWidth>
-                            予約
-                        </Button>
-                    </Grid>
-                </Box>
-                <div>
-                    <ButtonBase sx={{ width: "400px" }}>
-                        <Img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" />
-                    </ButtonBase>
-                </div>
-            </Container >
-
-        </>
-    )
+            </Box>
+            <div>
+              <ButtonBase sx={{ width: "600px" }}>
+                <Img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" />
+              </ButtonBase>
+            </div>
+          </Container>
+        </StyledTabPanel>
+      </Tabs >
+    </>
+  );
 }
 
-
-export default page
