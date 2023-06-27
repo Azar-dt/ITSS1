3232d10a2d551b47aae4b6455eb975f6db11750c
congvt109 <congvubbk109@gmail.com>
2023-06-27 18:03:42 +0700

    UI: fix UI and mockData in Order Page

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,11 +1,9 @@
-/* eslint-disable import/no-unresolved */
-
 "use client";
 
 import Header from "@/components/Header";
 import { DeleteForeverRounded } from "@mui/icons-material";
+import { Button } from "@mui/material";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
@@ -19,85 +17,79 @@ import Typography from "@mui/material/Typography";
 import * as React from "react";
 
 interface Column {
-  id: "name" | "code" | "population" | "size" | "density";
+  id: string;
   label: string;
   minWidth?: number;
-  align?: "right";
+  align?: "center";
   format?: (value: number) => string;
 }
 
 const columns: readonly Column[] = [
-  { id: "name", label: "Name", minWidth: 170 },
-  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
-  {
-    id: "population",
-    label: "Population",
-    minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
-    minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "density",
-    label: "Density",
-    minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toFixed(2),
-  },
+  { id: "userName", label: "名前", minWidth: 100 },
+  { id: "bikeImg", label: "バイク写真", minWidth: 100 },
+  { id: "bikeName", label: "バイク名", minWidth: 100 },
+  { id: "price", label: "価格", minWidth: 100 },
+  { id: "startTime", label: "開始時間", minWidth: 100 },
+  { id: "endTime", label: "終了時間", minWidth: 100 },
+  { id: "status", label: "状況", minWidth: 100 },
+  { id: "delete", label: "削除", minWidth: 100 },
 ];
 
-interface Data {
-  name: string;
-  code: string;
-  population: number;
-  size: number;
-  density: number;
-}
-
-function createData(
-  name: string,
-  code: string,
-  population: number,
-  size: number
-): Data {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
+const imgURL =
+  // eslint-disable-next-line max-len, sonarjs/no-duplicate-string
+  "https://images.unsplash.com/photo-1508357941501-0924cf312bbd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW90b2Jpa2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80";
 
 const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("UnitedKingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
+  {
+    id: 1,
+    userName: "John",
+    bikeImg: imgURL,
+    bikeName: "Ducati",
+    price: 100000,
+    startTime: "11-06-2023",
+    endTime: "30-07-2023",
+    status: "Approved",
+  },
+  {
+    id: 2,
+    userName: "Anna",
+    bikeImg: imgURL,
+    bikeName: "Yamaha",
+    price: 123000,
+    startTime: "23-04-2023",
+    endTime: "30-05-2023",
+    status: "Cancelled",
+  },
+  {
+    id: 3,
+    userName: "Lenna",
+    bikeImg: imgURL,
+    bikeName: "Honda",
+    price: 233000,
+    startTime: "22-06-2023",
+    endTime: "25-07-2024",
+    status: "Pending",
+  },
+  {
+    id: 4,
+    userName: "Lenna",
+    bikeImg: imgURL,
+    bikeName: "Honda",
+    price: 233000,
+    startTime: "23-06-2023",
+    endTime: "30-06-2023",
+    status: "Pending",
+  },
+  {
+    id: 5,
+    userName: "Lenna",
+    bikeImg: imgURL,
+    bikeName: "Honda",
+    price: 233000,
+    startTime: "23-06-2023",
+    endTime: "30-06-2023",
+    status: "Pending",
+  },
 ];
 
 export default function Orders() {
@@ -115,10 +107,9 @@ export default function Orders() {
     setPage(0);
   };
 
-  function handleClick(id: string) {
-    // Define the logic for handling the button click here
+  function handleClick() {
     // eslint-disable-next-line no-console
-    console.log(`Button clicked for row with id ${id}`);
+    console.log("Button clicked for row ");
   }
   return (
     <>
@@ -141,17 +132,19 @@ export default function Orders() {
         <Paper sx={{ width: "100%", overflow: "hidden" }}>
           <TableContainer sx={{ maxHeight: 440 }}>
             <Table stickyHeader aria-label="sticky table">
-              {/* <TableHead>
+              <TableHead>
                 <TableRow>
-                  {columns.map((column) => (
-                    <TableCell
-                      key={column.id}
-                      align={column.align}
-                      style={{ minWidth: column.minWidth }}
-                    >
-                      {column.label}
-                    </TableCell>
-                  ))}
+                  {columns.map((column) => {
+                    return (
+                      <TableCell
+                        key={column.id}
+                        align="center"
+                        sx={{ backgroundColor: "#9DB2BF", fontWeight: "700" }}
+                      >
+                        {column.label}
+                      </TableCell>
+                    );
+                  })}
                 </TableRow>
               </TableHead>
               <TableBody>
@@ -159,92 +152,47 @@ export default function Orders() {
                   .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                   .map((row) => {
                     return (
-                      <TableRow
-                        hover
-                        role="checkbox"
-                        tabIndex={-1}
-                        key={row.code}
-                      >
-                        {columns.map((column) => {
-                          const value = row[column.id];
-                          return (
-                            <TableCell key={column.id} align={column.align}>
-                              {column.format && typeof value === "number"
-                                ? column.format(value)
-                                : value}
-                              <Button
-                                variant="contained"
-                                onClick={() => handleClick(row.id)}
-                              >
-                                Click me
-                              </Button>
-                            </TableCell>
-                          );
-                        })}
+                      <TableRow hover tabIndex={-1} key={row.id}>
+                        <TableCell align="center">{row.userName}</TableCell>
+                        <TableCell align="center">
+                          <Box
+                            component="img"
+                            sx={{
+                              width: "150px",
+                              borderRadius: "8px",
+                            }}
+                            alt="The bike"
+                            src={row.bikeImg}
+                          />
+                        </TableCell>
+                        <TableCell align="center">{row.bikeName}</TableCell>
+                        <TableCell align="center">{row.price}</TableCell>
+                        <TableCell align="center">{row.startTime}</TableCell>
+                        <TableCell align="center">{row.endTime}</TableCell>
+                        <TableCell align="center">
+                          <Typography
+                            variant="subtitle2"
+                            component="span"
+                            fontWeight={700}
+                            my={2}
+                            px={2}
+                            py={1}
+                          >
+                            {row.status}
+                          </Typography>
+                        </TableCell>
+                        <TableCell align="center">
+                          <Button
+                            variant="contained"
+                            onClick={() => handleClick()}
+                          >
+                            削除
+                            <DeleteForeverRounded fontSize="small" />
+                          </Button>
+                        </TableCell>
                       </TableRow>
                     );
                   })}
-              </TableBody> */}
-              <TableHead>
-                <TableRow>
-                  <TableCell align="center">ID</TableCell>
-                  <TableCell align="center">イメージ</TableCell>
-                  <TableCell align="center">バイクID</TableCell>
-                  <TableCell align="center">ユーザID</TableCell>
-                  <TableCell align="center">開始時間</TableCell>
-                  <TableCell align="center">終了時間</TableCell>
-                  <TableCell align="center">Status</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {columns.map((row) => (
-                  <TableRow key={row.id}>
-                    <TableCell align="center">ID</TableCell>
-                    <TableCell align="center">
-                      <Box
-                        component="img"
-                        sx={{
-                          width: "150px",
-                          borderRadius: "8px",
-                        }}
-                        alt="The bike"
-                        // eslint-disable-next-line max-len
-                        src="https://images.unsplash.com/photo-1508357941501-0924cf312bbd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW90b2Jpa2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
-                      />
-                    </TableCell>
-                    <TableCell align="center">1</TableCell>
-                    <TableCell align="center">2</TableCell>
-                    <TableCell align="center">12:00:00</TableCell>
-                    <TableCell align="center">15:00:00</TableCell>
-                    <TableCell align="center">
-                      <Box
-                        sx={{
-                          display: "flex",
-                          alignItems: "center",
-                          justifyContent: "center",
-                        }}
-                      >
-                        <Typography
-                          variant="h6"
-                          component="span"
-                          fontWeight={500}
-                          my={2}
-                          px={2}
-                          py={1}
-                        >
-                          Pending
-                        </Typography>
-                        <Button
-                          variant="contained"
-                          onClick={() => handleClick(row.id)}
-                        >
-                          削除
-                          <DeleteForeverRounded fontSize="small" />
-                        </Button>
-                      </Box>
-                    </TableCell>
-                  </TableRow>
-                ))}
               </TableBody>
             </Table>
           </TableContainer>
