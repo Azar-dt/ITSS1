fc058321ecf38e5001e69b02d01f52bf286c0fa1
Redgold <72465223+tmtumk@users.noreply.github.com>
2023-07-03 22:13:16 +0700

    Update UI

--- app/store/[id]/page.tsx
+++ app/store/[id]/page.tsx
@@ -1,15 +1,20 @@
+/* eslint-disable max-len */
+
 "use client";
 
 import Header from "@/components/Header";
+import CloseIcon from "@mui/icons-material/Close";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import TtyIcon from "@mui/icons-material/Tty";
-import { Box, Container, Grid } from "@mui/material";
+import { Box, Button, Container, DialogTitle, Grid } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
 import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
 import Typography from "@mui/material/Typography";
 
 import StoreBikeList, { BIKE_TYPES } from "@/components/StoreBikeList";
 import fetcher from "@/libs/fetcher";
+import Modal from "@mui/material/Modal";
 import { Store } from "@prisma/client";
 import * as React from "react";
 import useSWR from "swr";
@@ -22,10 +27,14 @@ interface TabPanelProps {
 
 export default function StorePage({ params }: { params: { id: string } }) {
   const [value, setValue] = React.useState(0);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { data, isLoading } = useSWR<Store>(`/api/store/${params.id}`, fetcher);
 
   const [cursor, setCursor] = React.useState(0);
@@ -41,6 +50,7 @@ export default function StorePage({ params }: { params: { id: string } }) {
       setBikeType(undefined);
     }
     setSelectedIndex(index);
+    // eslint-disable-next-line security/detect-object-injection
     setBikeType(BIKE_TYPES[index].name);
   };
 
@@ -130,6 +140,73 @@ export default function StorePage({ params }: { params: { id: string } }) {
                   />
                   電話番号: {data?.phoneNumber}
                 </Typography>
+                <div>
+                  <Button
+                    sx={{ marginTop: "30px" }}
+                    variant="contained"
+                    href="#contained-buttons"
+                    onClick={handleOpen}
+                  >
+                    評価を見る
+                  </Button>
+                  <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                  >
+                    <Box sx={style}>
+                      <DialogTitle id="id">
+                        <Box>
+                          <Grid container spacing={0.5}>
+                            <Grid item xs={11}>
+                              <Box
+                                sx={{
+                                  alignItems: "center",
+                                  // display: "flex",
+                                  textAlign: "center",
+                                }}
+                              >
+                                <Typography
+                                  variant="h4"
+                                  component="span"
+                                  fontWeight={600}
+                                  my={5}
+                                  // textAlign="center"
+                                >
+                                  評価
+                                </Typography>
+                                <Grid container spacing={0.5}>
+                                  <Grid item xs={2}>
+                                    <Box
+                                      component="img"
+                                      sx={{
+                                        width: "80%",
+                                        borderRadius: "8px",
+                                      }}
+                                      alt="The bike"
+                                      src={data?.imgUrl}
+                                    />
+                                  </Grid>
+                                  <Grid item xs={10} sx={{ textAlign: "left" }}>
+                                    <Typography>Lenna1</Typography>
+                                    <Typography>Lenna1</Typography>
+                                    <Typography>Lenna1</Typography>
+                                  </Grid>
+                                </Grid>
+                              </Box>
+                            </Grid>
+                            <Grid item xs={1}>
+                              <IconButton onClick={handleClose}>
+                                <CloseIcon />
+                              </IconButton>
+                            </Grid>
+                          </Grid>
+                        </Box>
+                      </DialogTitle>
+                    </Box>
+                  </Modal>
+                </div>
               </Grid>
 
               <Grid
@@ -197,3 +274,17 @@ function a11yProps(index: number) {
     "aria-controls": `simple-tabpanel-${index}`,
   };
 }
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 600,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  // display: "flex",
+  // justifyContent: "center",
+  // alignItems: "center",
+};
