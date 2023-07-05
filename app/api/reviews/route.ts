8e6d53a2a49ea98392a41f474adfe934bfc2ef06
Kai <ducthang9122001@gmail.com>
2023-07-06 00:31:47 +0700

    update store rating after review

--- app/api/reviews/route.ts
+++ app/api/reviews/route.ts
@@ -18,6 +18,40 @@ export async function POST(req: Request) {
       },
     });
 
+    // update store rating
+    const store = await prisma.store.findFirstOrThrow({
+      where: {
+        bikes: {
+          some: {
+            id: bikeId,
+          },
+        },
+      },
+    });
+
+    const allReviews = await prisma.review.findMany({
+      where: {
+        bike: {
+          storeId: store.id,
+        },
+      },
+    });
+
+    const totalRating = allReviews.reduce((acc, curr) => {
+      return acc + curr.rating;
+    }, 0);
+
+    const averageRating = totalRating / allReviews.length;
+
+    await prisma.store.update({
+      where: {
+        id: store.id,
+      },
+      data: {
+        rating: averageRating,
+      },
+    });
+
     return NextResponse.json(review);
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
